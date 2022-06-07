cfa7bc02bacc34c13f0216b2ebdd8fc09ff279e3
Marius Karstedt <info@marius-karstedt.de>
2022-06-07 23:28:53 +0200

    + added .env to gitignore + added test scenario

--- tests/test.py
+++ tests/test.py
@@ -25,9 +25,8 @@ def stop_selenium(driver):
     driver.close()
 
 
-def test_register():
+def test_register(driver):
     print("Registrieren test wird ausgeführt...")
-    driver = run_selenium()
 
     header = driver.find_element(By.TAG_NAME, "header")
     buttons = header.find_elements(By.TAG_NAME, "a")
@@ -41,13 +40,27 @@ def test_register():
     register_inputs[3].send_keys("password")
     register_container.find_element(By.TAG_NAME, "button").click()
 
-    # stop_selenium(driver)
     print("Test beendet")
 
 
-def test_feedback():
+def test_login(driver):
+    print("Login test wird ausgeführt...")
+
+    header = driver.find_element(By.TAG_NAME, "header")
+    buttons = header.find_elements(By.TAG_NAME, "a")
+    buttons[0].click()
+
+    register_container = driver.find_element(By.ID, "login")
+    register_inputs = register_container.find_elements(By.TAG_NAME, "input")
+    register_inputs[0].send_keys("tester@dhbwiki.de")
+    register_inputs[1].send_keys("password")
+    register_container.find_element(By.TAG_NAME, "button").click()
+
+    print("Test beendet")
+
+
+def test_feedback(driver):
     print("Feedback test wird ausgeführt...")
-    driver = run_selenium()
 
     container = driver.find_element(By.ID, "feedback")
 
@@ -64,15 +77,35 @@ def test_feedback():
     submit_button = container.find_element(By.TAG_NAME, "a")
     submit_button.click()
 
-    # stop_selenium(driver)
+    print("Test beendet")
+
+
+def test_delete_user(driver):
+    pass
+
+
+def test_register_login_delete(driver):
+    print("Das Test Szenario wird ausgeführt...")
+
+    test_register(driver)
+    driver.get("https://dhbwiki.de")
+    test_login(driver)
+    driver.get("https://dhbwiki.de")
+    test_delete_user(driver)
+
     print("Test beendet")
 
 
 if __name__ == '__main__':
-    test_type = input("Welcher test soll ausgeführt werden?\n1. Registrieren\n2. Feedback\nAntwort: ")
-    if test_type == '1':
-        test_register()
-    elif test_type == '2':
-        test_feedback()
+    test_type = input("Welcher test soll ausgeführt werden?\n1. Registrieren\n2. Feedback\n3. Registrieren -> Login -> Delete\nAntwort: ")
+    if test_type == '1' or test_type == '2':
+        driver = run_selenium()
+        if test_type == '1':
+            test_register(driver)
+        elif test_type == '2':
+            test_feedback(driver)
+        elif test_type == '3':
+            test_register_login_delete(driver)
+        stop_selenium(driver)
     else:
         print("Test not found")

5c88bb85b52dabe7400ddeebfdb4b2d85ae5e215
Marius Karstedt <info@marius-karstedt.de>
2022-06-09 16:06:25 +0200

    cleanup

--- restapi/src/routes/course.ts
+++ restapi/src/routes/course.ts
@@ -1,9 +1,4 @@
-import express from "express";
-import {attach_default_routes, init_router, mongo_get_all} from "../utils/utils";
-import {ErrorResponse} from "../utils/error_response";
-import {SuccessResponse} from "../utils/success_response";
-import {getDatabase} from "../database";
-import {ObjectId} from "mongodb";
+import {attach_default_routes, init_router} from "../utils/utils";
 
 const router = init_router("courses");
 
